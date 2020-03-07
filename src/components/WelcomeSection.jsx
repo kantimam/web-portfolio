e81fd83f55723ff496d4930ce7fd77a238b18829
Nyannazz <kantemir.imam@gmail.com>
2020-03-08 00:25:10 +0100

    welcomesection css fixed

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WelcomeSection = ({ scrollToPortfolio }) => {
     return (
         <section className="flexCenterAll centerText" id="welcomeSection">
-            <div>
+            <div className="welcomeWrapper flexCenterAll">
                 <h1>Kantemir Imamov</h1>
                 <h3>Full-Stack Developer</h3>
                 <button onClick={scrollToPortfolio} className="callToAction hoverScale">VIEW PROJECTS</button>
