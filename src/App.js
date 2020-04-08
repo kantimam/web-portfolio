7a481214812b964b57faeb909e5ef863aeb1b47a
Nyannazz <kantemir.imam@gmail.com>
2020-04-08 13:14:34 +0200

    pull request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import MetaBallsWrapper from './components/MetaBallsWrapper';
 import PortfolioSection from './components/PortfolioSection';
 import WelcomeSection from './components/WelcomeSection';
@@ -8,54 +8,59 @@ import Nav from './components/Nav';
 
 
 function App() {
-  const [showBG, setBG]=useState(true);
-  const [scrollPercent, setScroll]=useState(0);
+  const [showBG, setBG] = useState(true);
+  const [scrollPercent, setScroll] = useState(0);
 
-  const mainScrollRef=useRef(null);
-  const portfolioSectionRef=useRef(null);
+  const portfolioSectionRef = useRef(null);
 
   useEffect(() => {
-    const currentRef=mainScrollRef.current;
-    currentRef.addEventListener('scroll',handleScroll);
+    const scrollContainer = document;
+    scrollContainer.addEventListener('scroll', handleScroll);
     return () => {
-      currentRef.removeEventListener('scroll',handleScroll)
+      scrollContainer.removeEventListener('scroll', handleScroll)
     };
   }, [showBG, scrollPercent])
 
-  const handleScroll=(event)=>{
-    const scrollP=event.target.scrollTop / event.target.clientHeight;
+  const handleScroll = (event) => {
+    
+    const scrollP = window.scrollY / window.innerHeight;
+    console.log(scrollP);
     
     setScroll(scrollP);
     /* hide the webGL background if the second page is fully visible */
-    if(showBG && scrollP>=1){
+    if (showBG && scrollP >= 1) {
       setBG(false);
     }
-    else{
+    else {
       setBG(true);
     }
   }
 
-  const scrollToPortfolio=()=>{
+  const scrollToPortfolio = () => {
     portfolioSectionRef.current.scrollIntoView({
       behavior: 'smooth',
       block: 'start'
     })
   }
 
-  
 
-  const scrollToOpacity=scrollPercent>1? 1 : scrollPercent*scrollPercent;
+
+  const scrollToOpacity = scrollPercent > 1 ? 1 : scrollPercent * scrollPercent;
   return (
-    <div className="App">
-      {showBG&&
-        <MetaBallsWrapper/>
+    <>
+      {showBG &&
+        <MetaBallsWrapper />
       }
-      <main style={{backgroundColor: `rgba(240, 240, 240, ${scrollToOpacity})`}} ref={mainScrollRef}>
-        <Nav/>
-        <WelcomeSection scrollToPortfolio={scrollToPortfolio}/>
-        <PortfolioSection ref={portfolioSectionRef}/>
-      </main>
-    </div>
+      <div className="App">
+
+        <main style={{ backgroundColor: `rgba(240, 240, 240, ${scrollToOpacity})` }}>
+          <Nav />
+          <WelcomeSection scrollToPortfolio={scrollToPortfolio} />
+          <PortfolioSection ref={portfolioSectionRef} />
+        </main>
+      </div>
+    </>
+
   );
 }
 
