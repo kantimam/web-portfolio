0267b589de2449d0d0cb31cb9a3ca624f67d4bcf
Nyannazz <kantemir.imam@gmail.com>
2020-04-20 22:51:48 +0200

    build

---
+++ build/precache-manifest.6dcee81ddd1015265032f50cac818274.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c12a6d6850f35d61f50a2fdb3d8998e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a94df767b43f430c55",
+    "url": "/static/css/main.f28fd822.chunk.css"
+  },
+  {
+    "revision": "8f79d79022961b854f6c",
+    "url": "/static/js/2.87bc1893.chunk.js"
+  },
+  {
+    "revision": "e3a94df767b43f430c55",
+    "url": "/static/js/main.8f1f23d6.chunk.js"
+  },
+  {
+    "revision": "d7d299ccad0a72b8ea42",
+    "url": "/static/js/runtime-main.e7eb2258.js"
+  },
+  {
+    "revision": "f0bdffcc16de9480e9333f54a71c853b",
+    "url": "/static/media/aStarDone.f0bdffcc.png"
+  },
+  {
+    "revision": "cfdc9890858139b7872ebc4fc219e72e",
+    "url": "/static/media/monkey_main.cfdc9890.png"
+  },
+  {
+    "revision": "e997d4669825f8be2dc49fe28881a110",
+    "url": "/static/media/movie_main.e997d466.png"
+  },
+  {
+    "revision": "62638ad4ebd018cb8b4d049130b931dd",
+    "url": "/static/media/npm-logo-red.62638ad4.svg"
+  },
+  {
+    "revision": "23c30a60cf7ec94b3d62a98b789cffcf",
+    "url": "/static/media/threeMaze.23c30a60.png"
+  },
+  {
+    "revision": "33d15b87b6476769f905813da0412ea8",
+    "url": "/static/media/yt_main.33d15b87.png"
+  }
+]);
